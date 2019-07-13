18124e42374f075221c395e0e6332972ee658133
Robert Worman <tr3ppy@gmail.com>
2019-07-13 17:03:48 +1000

    Create public about route using new layout

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import DefaultLayout from './layouts/Default.vue'
+import PublicLayout from './layouts/Public.vue'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    // Application core routes
     {
       path: '/',
       component: DefaultLayout,
@@ -16,9 +19,17 @@ export default new Router({
           path: '',
           name: 'home',
           component: Home
-        },
+        }
+      ]
+    },
+
+    // Public about page
+    {
+      path: '/about',
+      component: PublicLayout,
+      children: [
         {
-          path: '/about',
+          path: '',
           name: 'about',
           component: About
         }
