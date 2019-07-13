b1eb1eda24babab32008c7b5ecfbf80f5041063f
Robert Worman <tr3ppy@gmail.com>
2019-07-13 21:40:00 +1000

    Add parallax component

--- src/layouts/Public.vue
+++ src/layouts/Public.vue
@@ -8,6 +8,14 @@
       </q-toolbar>
     </q-header>
 
+    <q-parallax src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <h2 class="text-white">Recipe Buddy</h2>
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn color="primary" label="Sign Up" />
+        <q-btn color="secondary" label="Log In" />
+      </div>
+    </q-parallax>
+
     <q-page-container>
       <router-view />
     </q-page-container>
